e39aeae18f55c56d82b27cef122235ed1e0f766b
rafaelmasselli <rafaelmasselli0@gmail.com>
2023-11-09 13:35:59 -0300

    feat: creates video frame logic to decode the vide

--- initial-template/app/pages/file-upload/src/app.js
+++ initial-template/app/pages/file-upload/src/app.js
@@ -1,37 +1,45 @@
 import Clock from "./deps/clock.js";
-import View from "./deps/view.js";
+import View from "./view.js";
 const view = new View();
 const clock = new Clock();
-let took = "";
 
 const worker = new Worker("./src/worker/worker.js", {
   type: "module",
 });
-
+worker.onerror = (error) => {
+  console.error("error worker", error);
+};
 worker.onmessage = ({ data }) => {
   if (data.status !== "done") return;
   clock.stop();
-  view.updateElapsedTIme(`Process took ${took.replace("ago", "")}`);
+  view.updateElapsedTime(`Process took ${took.replace("ago", "")}`);
 };
 
-worker.postMessage("enviado do pai");
-
-view.configureOnFIleChange((file) => {
-  worker.postMessage({
-    file,
-  });
+let took = "";
+view.configureOnFileChange((file) => {
+  const canvas = view.getCanvas();
+  worker.postMessage(
+    {
+      file,
+      canvas,
+    },
+    [canvas]
+  );
 
   clock.start((time) => {
     took = time;
-    view.updateElapsedTIme(`Process started ${time}`);
+    view.updateElapsedTime(`Process started ${time}`);
   });
 });
-
 async function fakeFetch() {
   const filePath = "/videos/frag_bunny.mp4";
-  const response = await fetch(filePath, {
-    method: "HEAD",
-  });
+  const response = await fetch(filePath);
+  // traz o tamanho do arquivo!
+  // const response = await fetch(filePath, {
+  //     method: "HEAD"
+  // })
+  // response.headers.get('content-length')
+  // debugger
   const file = new File([await response.blob()], filePath, {
     type: "video/mp4",
     lastModified: Date.now(),
